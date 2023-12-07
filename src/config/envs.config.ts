450683a6f2b025ead11295eea77a9e2807663068
Juudini <juudinidev@gmail.com>
2023-12-07 19:54:18 -0300

    refactor(config/envs): updated environment variables, removed unused ones

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -3,6 +3,7 @@ import { get } from "env-var";
 
 export const envs = {
     PORT: get("PORT").required().asPortNumber(),
-    MONGODB_USER: get("MONGO_USER").required().asString(),
-    MONGODB_PASSWORD: get("MONGO_PASSWORD").required().asString()
+    NODE_ENV: get("NODE_ENV").required().asString(),
+    MONGODB_URI: get("MONGODB_URI").required().asString(),
+    DB_NAME: get("DB_NAME").required().asString()
 };
